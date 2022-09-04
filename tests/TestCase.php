79019d884095693c58ff7e96bf57dc4f29ead1a8
sakhawathossensadi <newtonss919@gmail.com>
2022-09-05 00:50:11 +0600

    config TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Analyzen\Admin\Tests;
+
+use Analyzen\Admin\Models\Admin;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Laravel\Passport\PassportServiceProvider;
+use Analyzen\Admin\ServiceProvider;
+
+class TestCase extends OrchestraTestCase
+{
+    use WithFaker;
+
+    protected $candidate;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations();
+        $this->artisan('migrate')->run();
+
+        $candidate = Candidate::factory()->create();
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $guards = $app['config']->get('auth.guards');
+
+        $guards['api'] = [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ];
+
+        $app['config']->set('auth.guards', $guards);
+
+        $app['config']->set('auth.defaults.guard', 'api');
+
+        $app['config']->set('auth.providers.users.model', Candidate::class);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Analyzen\\Auth\\ServiceProvider',
+            'Analyzen\\Auth\\AuthServiceProvider',
+            PassportServiceProvider::class,
+            ServiceProvider::class,
+        ];
+    }
+}
