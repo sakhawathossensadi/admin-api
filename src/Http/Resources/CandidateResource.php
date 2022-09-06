fb052ab46ac26d1b510ab08e4d867a8eed4c2d70
sakhawathossensadi <newtonss919@gmail.com>
2022-09-07 00:20:42 +0600

    Candidate resource

---
+++ src/Http/Resources/CandidateResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Analyzen\Admin\Http\Resources;
+
+use Analyzen\Candidate\Http\Resources\CandidateResource as ResourcesCandidateResource;
+
+class CandidateResource extends ResourcesCandidateResource
+{
+}
