5dc31f12787b4472e635f5ae1aff8ce8e7d72e69
sakhawathossensadi <newtonss919@gmail.com>
2022-09-07 00:20:20 +0600

    method for get candidates

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -2,12 +2,18 @@
 
 namespace Analyzen\Admin\Http\Controllers;
 
+use Analyzen\Admin\Http\Resources\CandidateResource;
+use Analyzen\Admin\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends BaseController
 {
     public function index(Request $request)
     {
-        dd($request->user());
+        $query = User::candidate();
+
+        $candidates = $query->paginate();
+
+        return CandidateResource::collection($candidates);
     }
 }
