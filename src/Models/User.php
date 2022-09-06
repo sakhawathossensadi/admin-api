413b36218d1937d5a4f8f1ec2ce3190170346630
sakhawathossensadi <newtonss919@gmail.com>
2022-09-07 00:19:46 +0600

    scope for get candidates

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,6 +6,13 @@ use Analyzen\Auth\Models\User as AuthUser;
 
 class User extends AuthUser
 {
+    public function scopeCandidate($query)
+    {
+        $tableName = (new User())->getTable();
+
+        return $query->where("$tableName.role", self::CANDIDATE);
+    }
+
     protected static function newFactory()
     {
         return \Analyzen\Admin\Database\Factories\UserFactory::new();
