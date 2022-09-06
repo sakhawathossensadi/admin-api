474bbaee179f44b9c94009c74e9a8454ac52a21d
sakhawathossensadi <newtonss919@gmail.com>
2022-09-06 23:21:06 +0600

    add AdminApiTest file

---
+++ tests/Features/AdminApiTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Analyzen\Admin\Tests\Features;
+
+use Analyzen\Admin\Tests\TestCase;
+
+class AdminApiTest extends TestCase
+{
+    public function tests_get_candidates()
+    {
+        $this->withoutExceptionHandling();
+    }
+}
