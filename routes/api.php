22869311bb137c9680b2c6d465a0cdce47ff5113
sakhawathossensadi <newtonss919@gmail.com>
2022-09-11 16:35:58 +0600

    api for admin profile

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::group(
     ],
     static function (): void {
         Route::get('candidates', [AdminController::class, 'index'])->name('candidate.index');
+        Route::get('profile', [AdminController::class, 'profile'])->name('admin.profile');
         Route::post('candidates/{candidateId}/status', [AdminController::class, 'updateStatus'])->name('candidate.status.update');
         Route::get('candidates/{candidateId}/details', [AdminController::class, 'show'])->name('candidate.details');
     }
